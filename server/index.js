86905f165ada010e9b340064f38b7092d3414e3e
Nikole <nikole.waintraub@gmail.com>
2022-12-13 22:35:00 +0200

    cors

--- server/index.js
+++ server/index.js
@@ -2,10 +2,12 @@ import mongoose from "mongoose";
 import express from 'express';
 import { router } from "./routes.js";
 import Configuration from './Configuration.js';
+import cors from 'cors';
 
 const app = express();
 
 app.use(express.json());
+app.use(cors())
 
 const {MONGO_USER_NAME, MONGO_PASSWORD, MONGO_CLUSTER, MONGO_DB_NAME} = Configuration;
 
