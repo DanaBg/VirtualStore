e9bc635ca37ad5569f03b1c3a4299370a10e8596
Nikole <nikole.waintraub@gmail.com>
2022-12-13 22:38:24 +0200

    remove unused imports

--- client/src/screens/ShoppingCart/ShoppingCart.js
+++ client/src/screens/ShoppingCart/ShoppingCart.js
@@ -3,10 +3,8 @@ import Button from '@mui/material/Button';
 import Dialog from '@mui/material/Dialog';
 import DialogActions from '@mui/material/DialogActions';
 import DialogContent from '@mui/material/DialogContent';
-import DialogContentText from '@mui/material/DialogContentText';
 import DialogTitle from '@mui/material/DialogTitle';
-import ProductRow from '../StorePage/ProductRow';
-import CartItem from '../../CartItem';
+import CartItem from './CartItem';
 
 
 const ShoppingCart = (props) => {
